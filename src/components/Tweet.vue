6cb8ca0319504b58b3b107223ca94ff9b6306166
Arjansho1~ <arjansho@amazon.de>
2023-08-11 18:52:51 +0200

    Commit

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,41 +1,38 @@
-<template>
-  <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
+<script setup>
+import ProfileImage from './profileimage.vue'
+import User from './User.vue'
+import Timestamp from './Timestamp.vue'
+import Message from './Message.vue'
+import Actions from './Actions.vue'
 
+const props = defineProps ({
+        name: String, 
+        image: String, 
+        handle: String,
+        timestamp: String,
+        message: String
+})
+
+</script>
+<template>
+    <div class="tweet">
+        <ProfileImage :image="props.image"/>
     <div class="body">
       <div class="top">
-        <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
-        </span>
-
-        <span class="timestamp">Nov 30, 2020</span>
-      </div>
-
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
-
-      <div class="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+        <User userData="user" :name="props.name" :handle="props.handle"/>
+        <Timestamp :timestamp="props.timestamp"/>
+        </div>
+        <Message :message="props.message"/>
+        <Actions />
+    <div>
+        <i class="fas fa-ellipsis-h"></i>
     </div>
-
-    <i class="fas fa-ellipsis-h"></i>
-  </div>
+    </div>
+    </div>
+    
 </template>
-
-<style scoped>
+<style>
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
